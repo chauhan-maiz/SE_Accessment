0cee66b195bfc531515c3d0adf1e2a2629d97499
chauhan-maiz <musaibc3@gmail.com>
2024-07-02 17:12:59 +0530

    Update Food_Billing_System.c

--- Food_Billing_System.c
+++ Food_Billing_System.c
@@ -3,10 +3,11 @@
 #include<stdio.h>
 main()
 {
-	int n,amt,q,total_amt;
+	int n,amt,q,total_amt=0;
 	char ch;
 	do
 	{
+		printf("\n\n\t---------------Menu-----------------");
 		printf("\n\n\t 1.Pizza\t\tPrice = 180rs/pcs");
 		printf("\n\n\t 2.Burger\t\tPrice = 100rs/pcs");
 		printf("\n\n\t 3.Dosa\t\t\tPrice = 120rs/pcs");
@@ -18,36 +19,34 @@ main()
 		switch(n)
 		{
 			case 1:
-				printf("\n\n\t You have selected Pizza.");
-				amt=180;
+				printf("\n\n\t You have selected Pizza.");				
 				printf("\n\n\t Enter the quantity: ");
 				scanf("%d",&q);
-				total_amt=180*q;
+				amt=180*q;
 				break;
 			case 2:
 				printf("\n\n\t You have selected Burger.");
-				amt=100;
 				printf("\n\n\t Enter the quantity: ");
 				scanf("%d",&q);
-				total_amt=100*q;
+				amt=100*q;
 				break;
 			case 3:
 				printf("\n\n\t You have selected Dosa.");
-				amt=120;
 				printf("\n\n\t Enter the quantity: ");
 				scanf("%d",&q);
-				total_amt=120*q;
+				amt=120*q;
 				break;
 			case 4:
 				printf("\n\n\t You have selected Idli.");
-				amt=50;
+				
 				printf("\n\n\t Enter the quantity: ");
 				scanf("%d",&q);
-				total_amt=50*q;
+				amt=50*q;
 				break;
 			default:
 				printf("\n\n\t Invalid Choise.");
 		}
+		total_amt=total_amt+amt;
 		
 		printf("\n\n\t Amount : %d",amt);
 		printf("\n\n\t Total Amount : %d",total_amt);
